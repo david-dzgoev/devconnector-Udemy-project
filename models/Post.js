67a8c326df2f33ed141ed7c8b321af502a35522a
David Dzgoev <David.Dzgoev@libertymutual.com>
2020-02-17 16:38:04 -0500

    Creating Post mongoose model and adding private route to add a post

---
+++ models/Post.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+//Gonna create a variable called Schema so that later on we can say Schema instead of mongoose.Schema
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  //We want posts to be connected to a user so we create a reference to a user
+  user: {
+    type: Schema.Types.ObjectId,
+    //Reference the users model
+    ref: 'users'
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  //This is the name of the user. Reason we are doing this is that we want the possibility to keep posts even if a user is deleted. So we won't have to dig into the users collection in that case
+  name: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  likes: [
+    //This will be an array of user objects that will have ids
+    {
+      //This way we know which likes came from which user. A single user can only like a certain post once
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      }
+    }
+  ],
+  comments: [
+    //This will be an array of user objects that will have ids
+    {
+      //This way we know which comments came from which user.
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
+  date: {
+    //This is the date on the actual post itself.
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Post = mongoose.model('post', PostSchema);
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,55 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
 
-// @route   GET api/posts
-// @desc    Test route
-// @access  Public
-router.get('/', (req, res) => res.send('Posts route'));
+const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// @route   POST api/posts
+// @desc    Create a post
+// @access  Private because you have to be logged in to create a post
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('text', 'Text is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      //The reason we're getting the user is so that we can get the name, the avatar, and the user itself. The model of the post has a user itself in the post, we also need to name and avatar. These fields are going to come from the DB as it is not something we are sending with the request.
+      //We don't want to send the password back so do the .select
+      const user = await User.findById(req.user.id).select('-password');
+
+      //new object for a post
+      const newPost = new Post({
+        text: req.body.text,
+        //the rest of this stuff comes from the user
+        name: user.name,
+        avatar: user.avatar,
+        //user object in post model is a user's id
+        user: req.user.id
+      });
+
+      //So that we have the saved post object in an object so that we can send it as a response.
+      const post = await newPost.save();
+
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
